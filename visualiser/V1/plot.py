4bf55b487a5204fbb5079a4c3fe867765d09b273
jrobo-gith <jacktheroberts10@gmail.com>
2025-04-29 16:51:29 +0100

    Added profiles + UX

--- visualiser/V1/plot.py
+++ visualiser/V1/plot.py
@@ -1,24 +1,33 @@
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtWidgets, QtCore
-import numpy as np
-import sys
 
-class Plot():
+class Plot:
     """Class to plot and update plots at a time"""
-    def __init__(self, plot_allocation, init_x, init_y, args):
-        """Initialise plot"""
+    def __init__(self, plot_allocation, init_x, init_y, interval, args, data_func):
+        """
+        Initialise the plot
 
-        assert type(init_x) == np.ndarray, print("Initial x must be a numpy array")
-        assert type(init_y) == np.ndarray, print("Initial y must be a numpy array")
+        plot_allocation:    The subplot we're plotting to
+        init_x:             Initial x values
+        init_y:             Initial y values
+        interval:           How long between each update (ms)
+        args:               Cosmetic details about the graph
+        data_func:          (TEMPORARY) Simulating data
+        """
+
+        assert type(init_x) == list, print("Initial x must be a list")
+        assert type(init_y) == list, print("Initial y must be a list")
         assert type(args) == dict, print("Arguments must be a dictionary")
 
         self.plot_allocation = plot_allocation
         self.args = args
         self.init_x = init_x
         self.init_y = init_y
+        self.interval = interval
 
-        self.num_lines = init_x.shape[0]
-        print(self.num_lines)
+        self.num_lines = len(init_x)
+        print(f"Number of lines: {self.num_lines}")
+        self.data_gen = data_func
 
         if args["legend"]:
             self.plot_allocation.addLegend()
@@ -36,25 +45,24 @@ class Plot():
                            y=init_y[i],
                            line_name=args["line_names"][i],
                            symbol=args["symbols"][i],
-                           pen=args["pens"][i],)
+                           pen=pg.mkPen(color=args["pens"][i]['color'], width=args["pens"][i]['width']),)
             self.lines.append(self.line)
 
-        # self.timer = QtCore.QTimer()
-        # self.timer.setInterval(300)
-        # self.timer.timeout.connect(self.update_plot)
-        # self.timer.start()
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(self.interval)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
 
-    def plot_line(self, x:np.array, y:np.array, line_name:str, pen, symbol:list):
-        line = self.plot_allocation.plot(x, y, name=line_name, symbol=symbol[0], symbolSize=symbol[1], pen=pen)
+    def plot_line(self, x:list, y:list, line_name:str, pen, symbol:list):
+        line = self.plot_allocation.plot(x=x, y=y, name=line_name, symbol=symbol[0], symbolSize=symbol[1], pen=pen)
         return line
 
-    def update_plot(self, new_data_X, new_data_Y):
+    def update_plot(self):
         """Update plot"""
-        self.init_x = self.init_x[1:]
-        self.init_y = self.init_y[1:]
+        for i, self.line in enumerate(self.lines):
+            self.init_x[i] = self.init_x[i][1:]
+            self.init_y[i] = self.init_y[i][1:]
 
-        self.init_x.append(new_data_X)
-        self.init_y.append(new_data_Y)
-
-        for self.line in self.lines:
-            self.line.setData(self.init_x, self.init_y)
+            self.init_x[i].append(self.init_x[i][-1] + 1)
+            self.init_y[i].append(self.data_gen(self.init_x[i][-1]))
+            self.line.setData(self.init_x[i], self.init_y[i])
--- visualiser/V1/plotter.py
+++ visualiser/V1/plotter.py
@@ -3,53 +3,52 @@ from pyqtgraph.Qt import QtWidgets, QtCore
 import numpy as np
 import sys
 from plot import Plot
+import data_gen
+import matplotlib.pyplot as plt
+import json
 
+
+x1 = list(np.linspace(0, 50, 50))
+y1 = list(data_gen.sinusoid(x1))
+y2 = list(data_gen.tangent(x1))
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Universal Styling
-        uni_label = {"color": "black", "font-size": "15px"}
-        uni_title = {"color": "black", "font-size": "20px"}
-
-        example_plot_arg = {
-            ## Example arguments for each plot, some arguments are encased
-            # in a list to allow for multiple lines in one plot
-
-            # Titles
-            "title": "This title",
-            "label_title_x": "X-Axis",
-            "label_title_y": "Y-Axis",
-            "line_names": ["Bias", "Variance"],
-            # Plot Add-ons
-            "legend": True,
-            "grid": True,
-
-            # Line options
-            "pens": [pg.mkPen(color=(255, 0, 0), width=5, style=QtCore.Qt.DashLine)],
-            "symbols": [["+", 0]],
-
-            # Styles
-            "label_style": [uni_label],
-            "title_style": [uni_title],
-        }
+        with open('profiles/example.json') as f:
+            example = json.load(f)
+        with open('profiles/velocity.json') as f:
+            velocity = json.load(f)
+        with open('profiles/position.json') as f:
+            position = json.load(f)
 
         self.win = pg.GraphicsLayoutWidget(show=True, title="Visualiser")
         self.win.resize(1080, 720)
 
-        self.plot1 = self.win.addPlot(title="A")
-        self.plo1 = Plot(self.plot1, np.array([np.linspace(0, 10, 100)]),
-                         np.array([np.linspace(0, 10, 100), ]),
-                         args=example_plot_arg)
+        self.plot1 = self.win.addPlot(row=0, col=0, colspan=2)
+        self.plo1 = Plot(self.plot1,
+                            [x1],
+                            [y1],
+                            interval=300,
+                            args=example,
+                            data_func=data_gen.sinusoid)
 
-        self.plot2 = self.win.addPlot(title="B")
-        self.plot2 = Plot(self.plot2, np.array([np.linspace(0, 10, 100)]),
-                         np.array([np.linspace(0, 10, 100)]),
-                         args=example_plot_arg)
+        self.plot2 = self.win.addPlot(row=1, col=0)
+        self.plot2 = Plot(self.plot2,
+                            [x1],
+                            [y2],
+                            interval=100,
+                            args=position,
+                            data_func=data_gen.tangent)
 
-
-
+        self.plot3 = self.win.addPlot(row=1, col=1)
+        self.plot3 = Plot(self.plot3,
+                          [x1],
+                          [y2],
+                          interval=100,
+                          args=velocity,
+                          data_func=data_gen.tangent)
 
 
 app = QtWidgets.QApplication(sys.argv)
