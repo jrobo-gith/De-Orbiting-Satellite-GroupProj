ab1b35dcfffaa037c7407df8d6b74cfbb3604a19
jrobo-gith <jacktheroberts10@gmail.com>
2025-04-30 17:13:07 +0100

    Added grapher to GUI

---
+++ visualiser/V2/widgets/graph_stuff/graph_script.py
@@ -0,0 +1,58 @@
+import json
+
+import pyqtgraph as pg
+from visualiser.V2.widgets.graph_stuff.plot import Plot
+from visualiser.V2.widgets.graph_stuff import data_gen
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QWidget
+
+
+class Grapher(pg.GraphicsLayoutWidget):
+    def __init__(self, init_x, init_y):
+        super().__init__()
+        self.init_x = init_x
+        self.init_y = init_y
+
+        # Import profiles
+        with open('profiles/example.json') as f:
+            example = json.load(f)
+        with open('profiles/velocity.json') as f:
+            velocity = json.load(f)
+        with open('profiles/position.json') as f:
+            position = json.load(f)
+
+        self.plot_list = []
+
+        self.plot1 = self.addPlot(row=0, col=0, colspan=2)
+        self.plot1 = Plot(self.plot1,
+                          [self.init_x[0]],
+                          [self.init_y[0]],
+                          args=example,
+                          data_func=data_gen.sinusoid)
+        self.plot_list.append(self.plot1)
+
+        self.plot2 = self.addPlot(row=1, col=0)
+        self.plot2 = Plot(self.plot2,
+                          [self.init_x[1]],
+                          [self.init_y[1]],
+                          args=position,
+                          data_func=data_gen.tangent)
+        self.plot_list.append(self.plot2)
+
+        self.plot3 = self.addPlot(row=1, col=1)
+        self.plot3 = Plot(self.plot3,
+                          [self.init_x[2]],
+                          [self.init_y[2]],
+                          args=velocity,
+                          data_func=data_gen.cosine)
+
+        self.plot_list.append(self.plot3)
+
+    @QtCore.pyqtSlot(str, tuple)
+    def update_plots(self, name, update):
+        """MUST take in matrix of P X L, then MUST update each plot with a vector of 1 X L where:
+        L is the number of lines needing updates.
+        """
+        x, y = update
+        for i, plot in enumerate(self.plot_list):
+            plot.update_plot(x[i], y[i])
--- visualiser/V2/widgets/graph_stuff/graphs.py
+++ visualiser/V2/widgets/graph_stuff/graphs.py
@@ -1,12 +1,43 @@
-from PyQt5.QtWidgets import (
-    QWidget, QVBoxLayout)
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout)
+from PyQt5 import QtCore, QtGui
+import pyqtgraph as pg
 
 from visualiser.V2.partials.navbar import Navbar
+from visualiser.V2.widgets.graph_stuff.graph_script import Grapher
+from visualiser.V2.widgets.graph_stuff import data_gen
+
 import json
+import time
+import numpy as np
+import threading
 
 with open('partials/global_settings.json') as f:
     glob_setting = json.load(f)
 
+class Helper(QtCore.QObject):
+    changedSignal = QtCore.pyqtSignal(str, tuple)
+
+def create_data(helper, name):
+    """MUST OUTPUT Matrix of size P X L where:
+    P is the number of plots in the window and
+    L is the number of lines in the plot.
+    """
+    additional_x = list(np.arange(3.0, 500.0, 0.1))
+
+    for i in range(len(additional_x)):
+        outgoing_x = [[additional_x[i]], [additional_x[i]], [additional_x[i]]]
+        outgoing_y = [data_gen.sinusoid(outgoing_x[0]), data_gen.tangent(outgoing_x[1]), data_gen.cosine(outgoing_x[2])]
+        helper.changedSignal.emit(name, (outgoing_x, outgoing_y))
+        time.sleep(.1)
+
+init_x = [list(np.linspace(-3.0, 3.0, 100))]
+init_y = [data_gen.sinusoid(init_x[0])]
+
+init_x.append(list(np.linspace(-3.0, 3.0, 100)))
+init_y.append(data_gen.tangent(init_x[1]))
+
+init_x.append(list(np.linspace(-3.0, 3.0, 100)))
+init_y.append(data_gen.cosine(init_x[2]))
 
 class Graphs(QWidget):
     def __init__(self, stacked_widget):
@@ -17,10 +48,12 @@ class Graphs(QWidget):
         navbar = Navbar("Graphs", self.stacked_widget)
 
         container = QVBoxLayout()
-        graph_box = QWidget()
-        graph_box.setStyleSheet(f"background-color: rgb{glob_setting['background-color']};")
+        graph = Grapher(init_x=init_x, init_y=init_y)
 
         container.addWidget(navbar, stretch=1)
-        container.addWidget(graph_box, stretch=20)
-
+        container.addWidget(graph, stretch=20)
         self.setLayout(container)
+
+        helper = Helper()
+        helper.changedSignal.connect(graph.update_plots, QtCore.Qt.QueuedConnection)
+        threading.Thread(target=create_data, args=(helper, "redundant_name"), daemon=True).start()
