c12fc0c3361b0f98ae6c4a9407f1dff6c6065344
jrobo-gith <jacktheroberts10@gmail.com>
2025-05-03 10:56:45 +0100

    Navbar changes (better alignment

--- visualiser/V2/partials/navbar.py
+++ visualiser/V2/partials/navbar.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout,
-    QLabel, QStackedWidget, QMainWindow
+    QLabel, QStackedWidget, QMainWindow, QGridLayout
 )
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
@@ -9,26 +9,27 @@ with open('partials/global_settings.json') as f:
     glob_setting = json.load(f)
 
 class Navbar(QWidget):
-    def __init__(self, title:str, stacked_widget):
+    def __init__(self, name:str, stacked_widget):
         super().__init__()
 
-        self.title = title
+        self.name = name
         self.stacked_widget = stacked_widget
 
-        navbar = QHBoxLayout()
-        title = QLabel(self.title)
-        title.setAlignment(Qt.AlignHCenter)
-        title.setFont(QFont(glob_setting['font-family'], 30))
-        title.setStyleSheet(f"color: rgb{glob_setting['font-color']}; background-color: rgb(55, 255, 55);")
-
-        backarrow = QPushButton("<-")
-        backarrow.setStyleSheet(f"color: rgb{glob_setting['font-color']}; background-color: rgb(255, 255, 55);")
-        backarrow.setFont(QFont(glob_setting['font-family'], 30))
-        backarrow.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
-
-        navbar.addWidget(backarrow, stretch=1)
-        navbar.addWidget(title, stretch=50)
-
+        navbar = QGridLayout()
         self.setLayout(navbar)
+
+        title = QLabel(self.name)
+        title.setStyleSheet("background-color: white;")
+
+        backarrow = QPushButton("<--")
+        backarrow.setStyleSheet("background-color: white; text-align: left;")
+
+        filler = QLabel("FILLER")
+        filler.setStyleSheet(f"color: {glob_setting['background-color']};")
+
+        navbar.addWidget(title, 0, 1, Qt.AlignHCenter)
+        navbar.addWidget(backarrow, 0, 0, Qt.AlignHCenter)
+        navbar.addWidget(filler, 0, 2)
+
 
 
