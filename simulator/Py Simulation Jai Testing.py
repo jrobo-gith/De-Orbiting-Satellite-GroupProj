a6496fd5b62b542d366cac8904dddf3263f64964
Jai <jaisagoo@outlook.com>
2025-05-06 11:50:51 +0100

    Fixed and added true atmospheric density
    Uses the NRLMSISE-00 model. Also rearranged the program to be more clear and readable

--- simulator/Py Simulation Jai Testing.py
+++ simulator/Py Simulation Jai Testing.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+import pymsis
+import datetime
 
 np.random.seed(0)
 
@@ -17,27 +19,24 @@ M_SAT = 500                                 # Mass of the satellite (kg)
 RHO_0 = 1.225                               # Air density at sea level (kg/m^3)
 H_SCALE = 8500                              # Scale height of the atmosphere (m)
 
-# Calculates atmospheric density at a given altitude
-def atmospheric_density(altitude):
-    # White noise as random perturbations of magnitude 0.1*RHO_0
-    density = RHO_0 * np.exp(-altitude / H_SCALE)
-    white_noise = np.random.normal(0, 0.05* density)
-    if altitude > 0:
-        return density + white_noise
-    else:
-        return 0
-    
-def atmospheric_density_true(altitude):
-    return
+# ------------------------------------------------------------------------------------------------------------------
+# ----------------------- FUNCTIONS FOR CALCULATING POSITION IN LATITUDE AND LONGITUDE -----------------------------
+# Functions with the suffix '_plot' use vectorized loops (for plotting)
+
+# Calculates the Earth's "radius" at the satellite's position
+# (Uses the WGS-84 ellipsoid model)
+# https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+def earth_radius_WGS84(latitude):
+    numerator = (EARTH_SEMIMAJOR**2 * np.cos(latitude))**2 + (EARTH_SEMIMINOR**2 * np.sin(latitude))**2
+    denominator = (EARTH_SEMIMAJOR * np.cos(latitude))**2 + (EARTH_SEMIMINOR * np.sin(latitude))**2
+    return np.sqrt(numerator / denominator)
 
 def curvature_in_prime_vertical(phi):
     return EARTH_SEMIMAJOR / np.sqrt(1 - E_SQUARED * np.sin(phi)**2)
 
-# Returns latitude and height above ellipsoid
-# Calculates latitude for the WGS-84 model using Bowring's method
+# Returns latitude and height above the Earth
+# Calculates latitude iteratively for the WGS-84 model using Bowring's method
 # RESEARCH INTO FERRARI'S METHOD FOR BETTER ACCURACY
-import numpy as np
-
 def latitude_iterator_and_height_plot(x, y, z):
     x = np.asarray(x)
     y = np.asarray(y)
@@ -66,7 +65,6 @@ def latitude_iterator_and_height_plot(x, y, z):
 
     return phi_final, height
 
-
 def latitude_iterator_and_height(x, y, z):
     r = np.sqrt(x**2 + y**2)
     phi = np.arctan(z / (r * (1 - E_SQUARED)))
@@ -83,26 +81,8 @@ def latitude_iterator_and_height(x, y, z):
     
     return phi_new, height
 
-# Calculates the latitude, longitude and altitude of the satellite
-
-# Calculates the Earth's "radius" at the satellite's position
-# (Uses the WGS-84 ellipsoid model)
-# https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
-def earth_radius_WGS84(latitude):
-    numerator = (EARTH_SEMIMAJOR**2 * np.cos(latitude))**2 + (EARTH_SEMIMINOR**2 * np.sin(latitude))**2
-    denominator = (EARTH_SEMIMAJOR * np.cos(latitude))**2 + (EARTH_SEMIMINOR * np.sin(latitude))**2
-    return np.sqrt(numerator / denominator)
-
 # Calculates the height of the satellite above an elliptical Earth
 # Latitude is calculated as an approximation for a non-spherical Earth
-def lat_long_height(x, y, z):
-    r = np.sqrt(x**2 + y**2 + z**2)
-    longitude = np.arctan2(z, np.sqrt(x**2 + y**2))
-    latitude, height = latitude_iterator_and_height(x, y, z)
-    R_earth = earth_radius_WGS84(latitude)
-    height = r - R_earth
-    return latitude, longitude, height
-
 def lat_long_height_plot(x, y, z):
     r = np.sqrt(x**2 + y**2 + z**2)
     longitude = np.arctan2(z, np.sqrt(x**2 + y**2))
@@ -111,17 +91,56 @@ def lat_long_height_plot(x, y, z):
     height = r - R_earth
     return latitude, longitude, height
 
+def lat_long_height(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    longitude = np.arctan2(z, np.sqrt(x**2 + y**2))
+    latitude, height = latitude_iterator_and_height(x, y, z)
+    R_earth = earth_radius_WGS84(latitude)
+    height = r - R_earth
+    return latitude, longitude, height
+
+# ------------------------------------------------------------------------------------------------------------------
+# ------------------------- FUNCTIONS FOR CALCULATING ATMOSPHERIC DENSITY ------------------------------------------
+# Calculates atmospheric density at a given altitude
+def atmospheric_density(altitude):
+    # White noise as random perturbations of magnitude 0.1*RHO_0
+    density = RHO_0 * np.exp(-altitude / H_SCALE)
+    white_noise = np.random.normal(0, 0.05* density)
+    if altitude > 0:
+        return density + white_noise
+    else:
+        return 0
+
+# Python module documentation:
+# https://swxtrec.github.io/pymsis/index.html
+
+# Needs to have the msis21.parm file renamed and copied to the 
+# following filepath: 'C:\Users\jaisa\AppData\Local\packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311'
+# and renamed to 'sitms21.parm' for pymsis to work correctly
+
+# Atmospheric Density Calculation using NRLMSISE-00 Model
+# https://en.wikipedia.org/wiki/NRLMSISE-00
+# https://www.nrl.navy.mil/our-research/nrlmsise-00
+# (Only accurate when using the WGS-84 model for the Earth)
+def atmospheric_density_true(lat, long, altitude, time = None):
+    if time is None:
+        time = datetime.datetime.now()
+    
+    true_density = pymsis.calculate(time, long, lat, altitude)
+    
+    return true_density[0, 0]  # Total mass density is the [0,0]th index of the pymsis.calculate() outputs
+
 # Diff. eqns for satellite motion
 def satellite_dynamics(t, y):
     x, y_pos, z, vx, vy, vz = y
     r = np.sqrt(x**2 + y_pos**2 + z**2)
-    altitude = lat_long_height(x, y_pos, z)[2]
+    lat, long, altitude = lat_long_height(x, y_pos, z)
 
     # Gravity
     F_gravity = -G * M_EARTH / r**2
 
     # Drag
-    rho = atmospheric_density(altitude)
+    rho = atmospheric_density_true(lat, long, altitude)
     v = np.sqrt(vx**2 + vy**2 + vz**2)
     F_drag_x = -0.5 * rho * CD * A * v * vx / M_SAT
     F_drag_y = -0.5 * rho * CD * A * v * vy / M_SAT
@@ -156,7 +175,7 @@ def system_solver(t_span, initial_conditions, t_evals=1000):
 
 # Initial conditions
 altitude_initial = 300e3
-velocity_initial = 7800
+velocity_initial = 8100
 x0 = EARTH_SEMIMAJOR + altitude_initial
 y0 = 0
 z0 = 0
