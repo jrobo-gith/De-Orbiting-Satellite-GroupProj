838aec4e04fcfea682cd273c372d4e46ce9a9b7d
Vijay <150015592+vjanandh@users.noreply.github.com>
2025-05-15 10:52:22 +0100

    Changes to docstrings

--- windows/instructions.py
+++ windows/instructions.py
@@ -37,7 +37,8 @@ class Instructions(QWidget):
         outlining the instructions and structured using a Vertical Box Layout (QVBoxLayout) to produce the instructions
         window.
 
-        :param stacked_widget: Widget containing pages of the GUI, used to navigate back to the main menu.
+        Args:
+            stacked_widget: Widget containing pages of the GUI, used to navigate back to the main menu.
         """
         super().__init__()
         self.stacked_widget = stacked_widget
